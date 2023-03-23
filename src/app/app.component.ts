5755cd1c4d101685b6563e3b9bcfcb38e276b517
masistemas01 <masistemas01@ambacar.ec>
2023-03-22 20:55:21 -0500

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'claseVisualStudioCode';
+}
+
+//ENTIDADES DE PRODUCTO
+
+let CODIGO : number=0;
+let NOMBRE : string='';
+let PESO : number=0;
+let FAMILIA : string='';
+let FECHA_CADUCIDAD : string='';
+let UNIDAD : number=0;
+let LOTE : string='';
+let PVP : number=0;
+let COSTO : number=0;
+
+console.log('hola mundo');
+console.log('hola mundo');
+console.log('hola mundo');
